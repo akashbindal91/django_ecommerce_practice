b725816f923c0579f56be683b88d0ce249913cf1
Akash Bindal <akashbindal91@gmail.com>
2020-08-15 17:26:25 +0530

    added the query string on how will it perform

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from shop.models import Product, Category
+
 # Create your views here.
 
 
@@ -24,6 +25,15 @@ def allProdCat(request, c_slug=None):
 def ProdCatDetail(request, c_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=c_slug, slug=product_slug)
+        # print(product.query)
+        print(Product.objects.filter(category__slug=c_slug, slug=product_slug).query)
+
+        """ This above Query will perform as """
+        # SELECT * FROM `shop_product` INNER JOIN `shop_category` ON (`shop_product`.`category_id` = `shop_category`.`id`) WHERE (`shop_category`.`slug` = category-4 AND `shop_product`.`slug` = product-4) ORDER BY `shop_product`.`name` ASC
+
+        """ Explain category__slug=c_slug """
+        # from category table where slug = c_slug
+
     except Exception as e:
         raise e
 
