6473d52869944e4f99c142d1bb86d0011afcb854
Akash Bindal <akashbindal91@gmail.com>
2020-10-04 20:46:48 +0530

    24. Customer orders history and order list

--- order/urls.py
+++ order/urls.py
@@ -4,5 +4,7 @@ from order import views
 from django.urls import path
 app_name= 'order'
 urlpatterns = [
-    path('thanks/<int:order_id>/', views.thanks, name='thanks')
+    path('thanks/<int:order_id>/', views.thanks, name='thanks'),
+    path('history/', views.orderHistory, name='order_history'),
+    path('view_order/<int:order_id>/', views.viewOrder, name='view_order'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from order.models import Order
+from order.models import Order, OrderItem
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -11,5 +12,24 @@ def thanks(request, order_id):
 
     if order_id:
         customer_order = get_object_or_404(Order, id=order_id)
-    return render( request, 'thanks.html', {'customer_order' : customer_order})
+    return render(request, 'thanks.html', {'customer_order': customer_order})
 
+
+@login_required()
+def orderHistory(request):
+    if request.user.is_authenticated:
+        email = str(request.user.email)
+        order_details = Order.objects.filter(emailAddress=email)
+    return render(request, 'order/order_list.html', {'order_details': order_details})
+
+
+@login_required()
+def viewOrder(request, order_id):
+    if request.user.is_authenticated:
+        email = str(request.user.email)
+        order_details = Order.objects.get(
+            emailAddress=email, id=order_id)
+        order_items = OrderItem.objects.filter(order=order_details)
+
+
+    return render(request, 'order/order_details.html', {'order_details': order_details, 'order_items': order_items})
